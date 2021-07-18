5c99443af56b78927af237a4b711ec6008d10601
Marcus Lo <m._.l@outlook.com>
2021-07-18 11:36:55 +1000

    fix

--- frontend/src/screens/LandingScreen.js
+++ frontend/src/screens/LandingScreen.js
@@ -109,7 +109,7 @@ export default function LandingScreen() {
         {classes &&
           classes.map((c) => {
             return (
-              <SyncedTo onClick={() => window.open(c.alternateLink, '_blank')}>
+              <SyncedTo>
                 <LinkWrapper>
                   <Link
                     style={{ textDecoration: 'none', color: '#5540ea' }}
@@ -122,7 +122,9 @@ export default function LandingScreen() {
                   title='Access on site'
                   aria-label='Link to access class on site'
                 >
-                  <IconWrapper>
+                  <IconWrapper
+                    onClick={() => window.open(c.alternateLink, '_blank')}
+                  >
                     <ArrowForwardIcon />
                   </IconWrapper>
                 </Tooltip>
